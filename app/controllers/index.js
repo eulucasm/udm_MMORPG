cc6b888301440e3e10dfd8b104e1449eba561913
eulucasm <lucaspoletis@gmail.com>
2021-05-27 20:59:53 -0300

    implementação do express session

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,21 @@
 module.exports.index = function(application, req, res){
-	res.render('index');
+    res.render('index', { validacao: {} });
+}
+
+module.exports.autenticar = function(application, req, res){
+    
+    var dadosForm = req.body;
+
+    req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+    req.assert('senha', 'Senha não pode ser vazia').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render('index', { validacao: erros });
+        return;
+    }
+
+    res.send('tudo ok para criar a sessão');
+
 }
