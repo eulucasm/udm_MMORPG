f197c38edf58d75654f1bf57e657781c06aba84b
eulucasm <lucaspoletis@gmail.com>
2021-06-09 19:21:59 -0300

    incluindo e implementando o modulo Crypto

--- app/models/UsuariosDAO.js
+++ app/models/UsuariosDAO.js
@@ -1,3 +1,5 @@
+var crypto = require("crypto");
+
 function UsuariosDAO(connection){
 	this._connection = connection();
 		//o _ é uma convenção que indica pra quem utiliza o modulo que aquela variavel faz parte do contexto da função
@@ -7,7 +9,13 @@ function UsuariosDAO(connection){
 UsuariosDAO.prototype.inserirUsuario = function(usuario){
 	this._connection.open(function(err, mongoclient){//essa função esperar um callback, recebendo como primeiro parametro um erro, e o segundo parametro é o objeto de conexão
 		mongoclient.collection("usuarios", function(err, collection){//a função colection permite manipular os documentos dentro das coleções
+			
+			var senha_criptografada = crypto.createHash("md5").update(usuario.senha).digest("hex");
+
+			usuario.senha = senha_criptografada;
+
 			collection.insert(usuario);
+			
 			mongoclient.close();
 		});
 	
@@ -18,6 +26,11 @@ de jogo, ou retorna a tela de login*/
 UsuariosDAO.prototype.autenticar = function(usuario, req, res){
 	this._connection.open(function(err, mongoclient){
 		mongoclient.collection("usuarios", function(err, collection){
+			
+			var senha_criptografada = crypto.createHash("md5").update(usuario.senha).digest("hex");
+			usuario.senha = senha_criptografada;
+
+
 			collection.fidn(usuario).toArray(function(err, result){
 
 				if(result[0] != undefined){
